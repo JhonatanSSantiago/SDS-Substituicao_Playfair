f5c5d06575383ad0a90c4941a8bf212edab4347b
Jhonatan Santiago <jhonssantiago@gmail.com>
2022-03-22 10:03:17 -0300

    Verificação se msg é par ou impar

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var letras_alf = "ABCDEFGHIJLMNOPQRSTUVXYZ";
+const espaco = ' ';
+
+document.getElementById("btn_cifrar").addEventListener("click",
+    (evento)=>{
+        evento.preventDefault();
+        verificarMsg();            
+    }
+);
+
+
+function verificarMsg(){
+    var msg = document.getElementById("msg").value.toUpperCase();
+    if(msg.length%2 == 0){
+        console.log(msg.length + " par "+ msg);
+    }else{
+        console.log(msg.length + " impar "+ msg);
+    }
+    
+}
