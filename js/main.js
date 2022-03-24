9ca3ea20d452b29ab047252bb16d35a637fd66be
Jhonatan Santiago <jhonssantiago@gmail.com>
2022-03-24 16:50:30 -0300

    Controle de caracteres na chave

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ const espaco = ' ';
 document.getElementById("btn_cifrar").addEventListener("click",
     (evento)=>{
         evento.preventDefault();
-        verificarMsg();            
+        verificarMsg();
+        pegaChave();            
     }
 );
 
@@ -13,13 +14,33 @@ function verificarMsg(){
     var msg = document.getElementById("msg").value.toUpperCase();
     if(msg.length%2 == 0){
         console.log(msg.length + " par "+ msg);
+        letrasSequencias(msg);
 
     }else{
         console.log(msg.length + " impar "+ msg);
         msg = msg+"Z"
         console.log(msg);
+        letrasSequencias(msg);
     }
     
 }
 
 
+function letrasSequencias(msg){
+    var cont = "";
+    var cont2 = "";
+    for (var i=0; i <= msg.length; i++ ){  
+        
+    }
+
+}
+
+function pegaChave(){
+    var msg = document.getElementById("chave").value.toUpperCase();
+    if (msg.length > 5 ){
+        alert("Chave precisa ter no maximo 5 caracteres");
+    }
+    if (msg.length < 5) {
+        alert("Chave precisa ter no minimo 5 caracteres");
+    }
+}
