4c1fbb6273513ad56eb9139d7fdacb93c9fb8543
Jhonatan Santiago <jhonssantiago@gmail.com>
2022-03-26 11:40:57 -0300

    preenchendo matriz com chave

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
-var letras_alf = "ABCDEFGHIJLMNOPQRSTUVXYZ";
+var alf = [ ["A","B","C","D","E"],
+            ["F","G","H","I","J"],
+            ["L","M","N","O","P"],
+            ["Q","R","S","T","U"],
+            ["V","W","X","Y","Z"] ];
+
+ var alfabeto = "ABCDEFGHIJLMNOPQRSTUVWXYZ";
 const espaco = ' ';
+var matriz = [  [ , , , , ],
+                [ , , , , ],
+                [ , , , , ],
+                [ , , , , ],
+                [ , , , , ]];
+
 
 document.getElementById("btn_cifrar").addEventListener("click",
     (evento)=>{
         evento.preventDefault();
         verificarMsg();
-        pegaChave();            
+        pegaChave();        
     }
 );
 
@@ -13,13 +25,13 @@ document.getElementById("btn_cifrar").addEventListener("click",
 function verificarMsg(){
     var msg = document.getElementById("msg").value.toUpperCase();
     if(msg.length%2 == 0){
-        console.log(msg.length + " par "+ msg);
+     //   console.log(msg.length + " par "+ msg);
         letrasSequencias(msg);
 
     }else{
-        console.log(msg.length + " impar "+ msg);
+      //  console.log(msg.length + " impar "+ msg);
         msg = msg+"Z"
-        console.log(msg);
+      //  console.log(msg);
         letrasSequencias(msg);
     }
     
@@ -36,11 +48,39 @@ function letrasSequencias(msg){
 }
 
 function pegaChave(){
-    var msg = document.getElementById("chave").value.toUpperCase();
-    if (msg.length > 5 ){
+    var chave = document.getElementById("chave").value.toUpperCase();
+    if (chave.length > 5 ){
         alert("Chave precisa ter no maximo 5 caracteres");
     }
-    if (msg.length < 5) {
+    if (chave.length < 5) {
         alert("Chave precisa ter no minimo 5 caracteres");
     }
+    if(chave.length == 5){ //prenche primeira linha da matriz
+        col = 0;
+        for (var i = 0; i < chave.length; i++){ 
+            matriz[0][col] = chave[i];
+            col = col+1;
+            
+        }
+        completarMatriz();
+    }
+    console.log(matriz);
 }
+
+
+function completarMatriz(){
+    var chave = document.getElementById("chave").value.toUpperCase();
+    var alfabeto_new = "";
+    for (var i = 0; i < chave.length; i++ ){
+       // var letra_igual =  alfabeto.lastIndexOf(chave[i]);
+        if( alfabeto.lastIndexOf(chave[i])){
+            alf.splice(0,)
+        }            
+    }
+    console.log(alfabeto_new) 
+}
+
+
+function mostrar(){
+   
+}    
